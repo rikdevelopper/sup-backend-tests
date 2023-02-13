b95ebe19f37273a2722113efd673207711fe7a70
kossiericahialey <eric.ahialey@aceko.fr>
2023-02-13 14:21:18 +0100

    Commit passing test

--- src/client/use-case/find-by-id/FindByIdUseCaseImpl.ts
+++ src/client/use-case/find-by-id/FindByIdUseCaseImpl.ts
@@ -12,7 +12,7 @@ export class FindByIdUseCaseImpl implements FindByIdUseCase{
     searchById(id: number): ClientResponse {
         const client: Client = this.clientRepository.findById(id);
         return {
-            firstName: client.firstName + 'EEEE',
+            firstName: client.firstName,
             email: client.email,
             lastName: client.lastName
         } as ClientResponse;
